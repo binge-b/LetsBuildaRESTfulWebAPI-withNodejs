6ada6494e9c60273125f00b9602599197f97cd30
binge-b <jtqbk3670@gmail.com>
2024-01-18 23:47:59 +0900

    1st commit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 
 const express = require("express");
 const app = express();
-// サーバーをつくる。
-app.listen(3000, console.log("サーバーが開始されました"));
 
+// appにこれからjson形式を使いますよとお伝え
+app.use(express.json());
+
+// サーバーをつくる。
+app.listen(3000, () => console.log("サーバーが開始されました"));
+
+// expressにはミドルウェアとルートハンドラがある,ルートハンドラとはget,postなどに紐づく関数
+// クライアントに返すバックエンド側の処理
 app.get("/", (req, res) =>{
     res.send("RestfulAPIを作ってみよう!");
 });
 
-// お客様情報をサーバーに置いておく。
-// /api/customers/で取得できるようにしておくJson形式でかく
+//クライアントに返すWebAPIを作る, お客様情報をサーバーに置いておく。
+// /api/customers/で取得できるようにしておくJson形式{キー: 値}でかく
 const customers = [
     { title: "田中", id: 1},
     { title: "斎藤", id: 2},
@@ -18,14 +24,49 @@ const customers = [
     { title: "加藤", id: 5},
 ];
 
-// データを取得できるようにしよう(GETメソッド）
+// 統一インターフェースを作る、GET取得する、POST投稿する、PUT更新・アップロードする、DELETE削除するが統一さえたインターフェース
+// データを/api/customersで取得できるようにしよう(GETメソッド）
+// エンドポイントはconst customers = [];の中身、クライアントに返すもともとのデータのこと、アドレス可能性
 app.get("/api/customers", (req, res) => {
     res.send(customers);
 })
 
 // 固有のお客様情報を取得する
-// お客様ひとりひとりに振られたidで1人分ずつのデータを取れるようにしておく
-// paramsはパラメータの意味
+// GET:お客様ひとりひとりに振られたidで1人分ずつのデータを取れるようにしておく
+// find関数： custmerの変数に格納した配列の中身をint型としてパースして展開、paramsはパラメータの意味
 app.get("/api/customers/:id",(req, res) => {
-    const 
-})
+    const customer = customers.find((c) => c.id === parseInt(req.params.id));
+    res.send(customer);
+});
+
+// POST：データを送信（作成）してみよう
+// クライアントから/api/customersにデータを新しくPOST投稿する
+// 配列の長さlengthに１つ新しいデータを足す
+// 例えば、formボタンの送信ボタンから入力したデータをPOSTする方法など、今回はPOSTMANを使用する
+app.post("/api/customers", (req, res) => {
+    if (!req.body.title) {
+        res.status(400).send({ message: "タイトルがああ"});
+        return;
+    }
+    const customer = {
+        id: customers.length + 1,
+        title: req.body.title,
+    };
+    customers.push(customer);
+    res.send(customers);
+});
+
+// PUT:データを更新してみよう
+app.put("/api/customers/:id", (req, res) => {
+    const customer = customers.find((c) => c.id === parseInt(req.params.id));
+    customer.title = req.body.title;
+    res.send(customers);
+});
+
+// DELETE:データを削除してみよう
+app.delete("/api/customers/", (req, res) => {
+    const customer = customers.find((c) => c.id === parseInt(req.params.id));
+    const index = customers.indexOf(customer);
+    customers.splice(index, 1);
+    res.send(customer);
+});
